6dcf6865f25c9c8b5717766f14e6db93931bd520
Gregg <74693768+gjrsas@users.noreply.github.com>
2023-08-10 02:08:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP'];
+    var member = {
+        name: 'Code',
+        age: 20,
+        skills: skills
+    };
+    console.log(member.skills);
+    console.log(member.skills[2]);
+    console.log(member.skills[3]);
+}
